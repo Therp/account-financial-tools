b4de3dd7dbbf49ba17f0d9dc2b3bf64351b020a9
Giovanni Capalbo <giovanni@therp.nl>
2023-05-23 08:09:56 +0000

    [ADD] sequence in account move lines

---
+++ account_move_line_sequence/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Junglegym Accounting customizations",
+    "version": "12.0.1.0.0",
+    "author": "Therp BV",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "summary": "Accounting customizations for Junglegym",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_move_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ account_move_line_sequence/models/account_move_line.py
@@ -0,0 +1,10 @@
+# Copyright 2023 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+    _order = "sequence, date desc, name desc, id"
+
+    sequence = fields.Integer(default=16, index=True)
